f2b0d1ff5a0dcff6f049bbae574d63c25cf0bce2
root <jasong@chapman.edu>
2020-01-14 20:53:52 +0000

    Made a few changes

--- helloworld.cpp
+++ helloworld.cpp
@@ -6,5 +6,7 @@ using namespace std;
 int main()
 {
   cout << "Hello World cpsc 298" << endl;
+  cout << "I had to add this statement cause Rene said so." << endl;
+  cout << "adding another cout" << endl;
   return 0;
 }
